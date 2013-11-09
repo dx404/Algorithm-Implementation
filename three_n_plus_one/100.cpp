ca62dca3e46cecfce60637a901506d7cd4adc8df
Duo "Donald" Zhao <wisdom.to.pilot@gmail.com>
2013-11-09 01:27:09 -0500

    Create 100.cpp

---
+++ three_n_plus_one/100.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int down2one(int n){
+  int c = 1;
+  while (n > 1){
+    n = (n & 1) ? (3*n+1):(n/2);
+    c++;
+  }
+  return c;
+}
+
+// precondition i <= j
+int maxDown2one(int i, int j){
+  int cmax = 0;
+  for (int n = i; n <= j; n++){
+    int c = down2one(n);
+    if (c > cmax)
+      cmax = c;
+  }
+  return cmax;
+}
+
+int main(int argc, char * argv[]){
+  int i, j, count = 0;
+  while (cin >> i >> j){
+    count = (i<=j) ? maxDown2one(i, j):maxDown2one(j, i);
+    cout << i << " " << j << " " <<count << endl;
+  }
+  return 0;
+}
